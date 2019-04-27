fda538a4f271c9d5bff109418c663c780643cd57
Urmat Bekboev <bek.urmat@gmail.com>
2019-04-27 11:35:42 +0600

    User registration and login added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {NotificationContainer} from "react-notifications";
 import {Container} from "reactstrap";
-import {Route, Switch} from "react-router-dom";
+import {Route, Switch, withRouter} from "react-router-dom";
 
 import './App.css';
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+import {connect} from "react-redux";
+import {logoutUser} from "./store/actions/usersActions";
 
 function App() {
   return (
     <div className="App">
         <NotificationContainer/>
         <header>
-            {/*<Toolbar user={this.props.user} logout={this.props.logoutUser}/>*/}
+            <Toolbar user={this.props.user} logout={this.props.logoutUser}/>
         </header>
-        <Container style={{marginTop: '20px'}}>
+        <Container>
             <Switch>
                 <Route path="/" exact render={() => <h1>Some component</h1>} />
                 <Route render={() => <h1>Page Not Found</h1>} />
@@ -22,4 +25,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+    user: state.users.user
+});
+
+const mapDispatchToProps = dispatch => ({
+    logoutUser: () => dispatch(logoutUser())
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
