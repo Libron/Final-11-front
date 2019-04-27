731ac37da83004542276dab9433d91512762bd62
Urmat Bekboev <bek.urmat@gmail.com>
2019-04-27 17:46:11 +0600

    All routes included

--- src/App.js
+++ src/App.js
@@ -1,36 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from "react-redux";
 import {NotificationContainer} from "react-notifications";
-import {Container} from "reactstrap";
+import {Col, Container, Row} from "reactstrap";
 import {Route, Switch, withRouter} from "react-router-dom";
 
-import './App.css';
-import Toolbar from "./components/UI/Toolbar/Toolbar";
-import {connect} from "react-redux";
 import {logoutUser} from "./store/actions/usersActions";
+import {fetchCategories} from "./store/actions/categoriesActions";
+import {fetchProducts} from "./store/actions/productsActions";
 
-function App() {
-  return (
-    <div className="App">
-        <NotificationContainer/>
-        <header>
-            <Toolbar user={this.props.user} logout={this.props.logoutUser}/>
-        </header>
-        <Container>
-            <Switch>
-                <Route path="/" exact render={() => <h1>Some component</h1>} />
-                <Route render={() => <h1>Page Not Found</h1>} />
-            </Switch>
-        </Container>
-    </div>
-  );
+import ProductsPage from "./containers/ProductsPage/ProductsPage";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+import ProductDetails from "./containers/ProductDetails/ProductDetails";
+import NewProduct from "./containers/NewProduct/NewProduct";
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+import Sidebar from "./components/UI/Sidebar/Sidebar";
+
+import './App.css';
+import Spinner from "./components/UI/Spinner/Spinner";
+
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchCategories();
+    };
+
+    render() {
+        if (this.props.loading || !this.props.categories) {
+            return <Spinner />
+        }
+
+      return (
+        <div className="App">
+            <NotificationContainer/>
+            <header>
+                <Toolbar user={this.props.user} logout={this.props.logoutUser}/>
+            </header>
+            <Container>
+                <Row>
+                    <Col md={3} sm={12}>
+                        <Sidebar categories={this.props.categories} fetchHandler={this.props.fetchProducts} />
+                    </Col>
+                    <Col md={9} sm={12}>
+                        <Switch>
+                            <Route path="/" exact component={ProductsPage} />
+                            <Route path="/products" exact component={ProductsPage} />
+                            <Route path="/products/new" exact component={NewProduct} />
+                            <Route path="/products/:id" exact component={ProductDetails} />
+                            <Route path="/register" exact component={Register}/>
+                            <Route path="/login" exact component={Login}/>
+                            <Route render={() => <h1>Page Not Found</h1>} />
+                        </Switch>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+      );
+    }
 }
 
 const mapStateToProps = state => ({
-    user: state.users.user
+    user: state.users.user,
+    categories: state.categories.categories,
+    loading: state.categories.loading
 });
 
 const mapDispatchToProps = dispatch => ({
-    logoutUser: () => dispatch(logoutUser())
+    logoutUser: () => dispatch(logoutUser()),
+    fetchCategories: () => dispatch(fetchCategories()),
+    fetchProducts: (query) => dispatch(fetchProducts(query))
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
