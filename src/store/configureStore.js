27d32613013955eb8a6e029653fce501bbba66e5
Urmat Bekboev <bek.urmat@gmail.com>
2019-04-27 17:46:00 +0600

    code optimization

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,12 +5,16 @@ import thunkMiddleware from "redux-thunk";
 import {loadFromLocalStorage, saveToLocalStorage} from "./localStorage";
 
 import usersReducer from "./reducers/usersReducer";
+import productsReducer from "./reducers/productsReducer";
+import categoriesReducer from "./reducers/categoriesReducer";
 
 export const history = createBrowserHistory();
 
 const rootReducer = combineReducers({
     router: connectRouter(history),
-    users: usersReducer
+    users: usersReducer,
+    products: productsReducer,
+    categories: categoriesReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
