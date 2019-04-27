a0af1b4f4b106e8cb9ddd4c3cf58e36173690d1e
Urmat Bekboev <bek.urmat@gmail.com>
2019-04-27 11:13:27 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import {NotificationContainer} from "react-notifications";
+import {Container} from "reactstrap";
+import {Route, Switch} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NotificationContainer/>
+        <header>
+            {/*<Toolbar user={this.props.user} logout={this.props.logoutUser}/>*/}
+        </header>
+        <Container style={{marginTop: '20px'}}>
+            <Switch>
+                <Route path="/" exact render={() => <h1>Some component</h1>} />
+                <Route render={() => <h1>Page Not Found</h1>} />
+            </Switch>
+        </Container>
     </div>
   );
 }
